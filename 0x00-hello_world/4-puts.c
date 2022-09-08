17f7c356b684c2d070e1d68c85e229e002f5b686
Chukwudalu1 <chukwudalueze01@gmail.com>
2022-09-08 16:45:47 -0700

    task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#!/bin/bash
+#include <sudioh>
+
+/**
+ * *main - Printing
+ *
+ * Return: Always 0 (Successful)
+ */
+int main (void)
+{
+	puts("\" Programming is like building a multilingual puzzle" );
+		return(0);
+	}
