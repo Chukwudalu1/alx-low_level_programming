711385fd90a53d7926a2657c5a4d593044ff1f49
Chukwudalu1 <chukwudalueze01@gmail.com>
2022-09-08 21:53:43 -0700

    task 5:

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,8 +5,7 @@
 *this program prints some texts
 *Returns: 0
 */
-int main(void)
-{
+int main(void){
 	printf("with the proper grammar, the outcome is a piece of art, \n");
-	returns (0);
+	return (0);
 }
