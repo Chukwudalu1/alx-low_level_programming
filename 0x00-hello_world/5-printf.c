53bbb7034a93274ccf1a34690d57af770e352466
Chukwudalu1 <chukwudalueze01@gmail.com>
2022-09-09 02:32:20 -0700

    task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,9 +3,10 @@
 *main print function
 *Deacription: using main function
 *this program prints some texts
-*Returns: 0
+*return: (0)
 */
-int main(void){
+int main(void)
+{
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
