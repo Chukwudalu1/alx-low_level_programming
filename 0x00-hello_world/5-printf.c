26921c004bc39fe6f23a9ea718dfd7d82d03b602
Chukwudalu1 <chukwudalueze01@gmail.com>
2022-09-08 21:27:54 -0700

    task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,14 @@
-#include <studio.h>
+#include <stdio.h>
 
 /**
- *
- * Description: using the main function 
- * this program prints "programming is like a mmultilingual puzzle"
- * Returns: 0
- */
+* main - prins function
+*
+*Description: using main function
+*this program prints some texts
+Returns: 0
+*/
 int main(void)
-{
-	printf ("with proper gramma, but the outcome is a piece of art, \n");
-	returns (0);
-]	
+{	
+		printf("withe the proper grammar, but the outcome is a piece of art, \n");
+		returns (0);
+}
