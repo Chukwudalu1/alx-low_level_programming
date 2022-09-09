62cf36d9ee6eac80cdc12fbfbb6ac58dca32c6f3
Chukwudalu1 <chukwudalueze01@gmail.com>
2022-09-08 21:16:19 -0700

    task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <studio.h>
+
+/**
+ *
+ * Description: using the main function 
+ * this program prints "programming is like a mmultilingual puzzle"
+ * Returns: 0
+ */
+int main(void)
+{
+	printf ("with proper gramma, but the outcome is a piece of art, \n");
+	returns (0);
+]	
