39449d65924f3e41ce3e4c589cadc600bab1bc6d
Chukwudalu1 <chukwudalueze01@gmail.com>
2022-09-09 05:04:51 -0700

    task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 /**
- * main - proints the string in the put function
+ * main - prints the string in the put function
  *
  * Description: using the main function
- * this programme prints and returns 0
+ * this programme prints and returns 
  * Return: 0
  */
 int main (void)
@@ -15,9 +15,9 @@ int main (void)
 	long long lli;
 	float f;
 
-	printf("Size of a char:  %ld bytes(s) \n", sizeof(c));
-	printf("Size of an int:  %ld bytes(s)  \n", sizeof(i));
-	printf("Size of a long int:  %ld byte(s) \n", sizeof(li));
-	printf("Size of a long long int:  %ld byte(s) \n", sizeof(lli));
-	printf("Size of a float:  %ld byte(s) \n", sizeof(f));
+	printf( "Size of a char:  %ld bytes(s) \n", sizeof(c));
+	printf( "Size of an int:  %ld bytes(s)  \n", sizeof(i));
+	printf( "Size of a long int:  %ld byte(s) \n", sizeof(li));
+	printf( "Size of a long long int:  %ld byte(s) \n", sizeof(lli));
+	printf( "Size of a float:  %ld byte(s) \n", sizeof(f));
 	return(0);}
