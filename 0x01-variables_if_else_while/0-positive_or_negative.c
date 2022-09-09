a8c5ab0776973b6cf7d4e3861901621fcb139ce3
Chukwudalu1 <chukwudalueze01@gmail.com>
2022-09-09 11:57:34 -0700

    task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -21,13 +21,11 @@ int main(void)
 {
 		int n;
 
-
 		srand(time(0));
 
 		n = rand() - RAND_MAX / 2;
 
 		if (n < 0)
-
 		{
 			printf("%d is negative\n", n);	
 		} else if (n == 0)
@@ -39,4 +37,5 @@ int main(void)
 			printf("%d is positive\n", n);
 		}
 		return (0);
-		}
+
+}
