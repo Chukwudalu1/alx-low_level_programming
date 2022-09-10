a0ba6a754de5e53df83079b3b994de45a374bb80
Chukwudalu1 <chukwudalueze01@gmail.com>
2022-09-10 10:39:32 -0700

    task 8

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,23 +1,23 @@
 #include<stdio.h>
 /**
- * main - entry point
+ * main - entry point 
  *
- * function main - a program that prints all the numbers of base 16
+ * function main - a program that prints all the numbers of base 16.
  *
  * Return: 0
  */
 int main(void)
 {
-		char c;
+	char c;
 
-		for (c = '0'; c <= '9'; c++)
-		{
-				putchar(c);
-		}
-		for (c = 'a'; c <= 'f'; c++)
-		{
-				putchar(c);
-		}
-		putchar('\n');
-		return (0);
+	for (c = '0'; c <= '9'; c++)
+	{
+		putchar(c);
+	}
+	for (c = 'a'; c <= 'f'; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
 }
