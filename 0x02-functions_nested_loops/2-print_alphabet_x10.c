db7338ed0506c602504000c1a0f15dfbb7092de5
Chukwudalu1 <chukwudalueze01@gmail.com>
2022-09-13 15:43:25 -0700

    task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -3,12 +3,12 @@
  * print_alphabets_x10 - Print alphabets in lowercase 10 times 
  * Return: 0
  */
-void print_alphabets_x10(void)
+void print_alphabet_x10(void)
 {
 	int i = 0;
 	char c;
 
-	while (i , 10)
+	while (i < 10)
 	{
 		c = 'a';
 		while (c <= 'z')
