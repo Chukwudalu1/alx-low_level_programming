08acc6c8d4df108456d53240844f47583ad7d503
Chukwudalu1 <chukwudalueze01@gmail.com>
2022-09-19 16:01:15 -0700

    task 11

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -12,7 +12,7 @@ int main(void)
 		for (count = 0, total = 2772; total > 122;  count++)
 		{
 			myrand = (rand() % 125) + 1;
-			printf("%c", myrand;
+			printf("%c", myrand);
 					total -= myrand;
 					}
 					printf("%c", total);
