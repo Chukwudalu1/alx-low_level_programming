7043b246fc09a78bb8197a6b0acfa519546f680f
Chukwudalu1 <chukwudalueze01@gmail.com>
2022-09-26 11:38:34 -0700

    task 3

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -2,7 +2,7 @@
 
 /**
  *  *_strspn - gets the length of a prefix substring
- *  @s: string to evaluate 
+ *  @s: string to evaluate
  *  @accept: string containing the list of characters to match in s
  *
  *  Return: the number of bytes in the intial segment
@@ -17,7 +17,7 @@ unsigned int _strspn(char *s, char *accept)
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		flag = 0;
-		for (j = 0; accept[j] !='\0'; j++)
+		for (j = 0; accept[j] != '\0'; j++)
 		{
 			if (s[i] == accept[j])
 			{
