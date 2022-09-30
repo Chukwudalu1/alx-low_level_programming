a67be6ac53dcddda5170a2c956c5e2faf8035d2b
Chukwudalu1 <chukwudalueze01@gmail.com>
2022-09-30 13:07:31 -0700

    task 2

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,19 @@
+#include <stdio.>
+
+/**
+ * main - main function
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: on success 0
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	if (argc > 0)
+	{
+		for (i = 0; i < argc; i++)
+			printf("%s\n", argv[i]);
+	}
+	return (0);
+}
