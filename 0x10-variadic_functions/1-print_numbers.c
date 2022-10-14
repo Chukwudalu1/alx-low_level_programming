0532e7cb7b1511a7956190da5c6278c52edeed80
Chukwudalu1 <chukwudalueze01@gmail.com>
2022-10-14 10:43:53 -0700

    task 1

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,27 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+
+/**
+ * print_numbers - prints integers
+ * @: seperating string
+ * @n: number of integers
+ */
+void prrint_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list ap;
+	unsigned int count;
+
+	va_start(ap, n);
+	count = 0;
+	while (count < n)
+	{
+		printf("%d", va_arg(ap, int));
+		if (separator && count < n - 1)
+		{
+			printf("%s", separator);
+		}
+		count++;
+	}
+	printf("\n");
+	va_end(ap);
+}
